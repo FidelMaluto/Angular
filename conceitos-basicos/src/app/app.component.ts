4d5869e63000b18db73c8efa8cce69a3ef63fea8
Fidel Maulto <fidel.maluto77@gmail.com>
2026-06-23 00:25:51 +0100

    refactor: Editando algumas coisas simples no component mypagina

--- conceitos-basicos/src/app/app.component.ts
+++ conceitos-basicos/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloWorldComponent } from './helloworld/helloworld.component';
+import { MypaginaComponent } from "./mypagina/mypagina.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloWorldComponent],
+  imports: [RouterOutlet, HelloWorldComponent, MypaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

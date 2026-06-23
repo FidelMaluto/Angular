dceee01e695d82f2d650b565c9ba0ccc67262497
Fidel Maulto <fidel.maluto77@gmail.com>
2026-06-23 23:51:29 +0100

    feat: Criando a estrutura da calculadora

--- conceitos-basicos/src/app/app.component.ts
+++ conceitos-basicos/src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HelloWorldComponent } from './helloworld/helloworld.component';
-import { MypaginaComponent } from "./mypagina/mypagina.component";
+import { CalculadoraComponent } from './calculadora/calculadora.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloWorldComponent, MypaginaComponent],
+  imports: [CalculadoraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

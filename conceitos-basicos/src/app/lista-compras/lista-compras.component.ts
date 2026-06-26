b6f3095e17a6f7a9f844e4276f582e1490ddd7cc
Fidel Maulto <fidel.maluto77@gmail.com>
2026-06-26 16:16:04 +0100

    feat: Adicionando a funcionalidade de riscar os itens comprados

--- conceitos-basicos/src/app/lista-compras/lista-compras.component.ts
+++ conceitos-basicos/src/app/lista-compras/lista-compras.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ListaItem } from './lista-item';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [ FormsModule ],
+  imports: [FormsModule, CommonModule],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss'
 })
@@ -13,15 +14,23 @@ export class ListaComprasComponent {
   item: string = '';
   lista: ListaItem[] = [];
 
-  adicionarItem(){
+  adicionarItem() {
     let listaItem = new ListaItem();
     listaItem.nome = this.item;
     listaItem.id = this.lista.length + 1;
 
     this.lista.push(listaItem);
- 
+
     this.item = '';
     console.table(this.lista);
   }
-      
+
+  riscarItem(listaItem: ListaItem) {
+    listaItem.comprado = !listaItem.comprado;
+  }
+
+  limparLista() {
+    this.lista = [];
+  }
+
 }

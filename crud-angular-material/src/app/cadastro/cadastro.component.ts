964592ea09fc961e0ba234697fe05c047ca8c00e
Fidel Maulto <fidel.maluto77@gmail.com>
2026-07-22 11:33:33 +0100

    feat: Criando services

--- crud-angular-material/src/app/cadastro/cadastro.component.ts
+++ crud-angular-material/src/app/cadastro/cadastro.component.ts
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { Cliente } from './cliente';
+import { ClienteService } from '../cliente.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -26,8 +27,12 @@ export class CadastroComponent {
 
   cliente: Cliente = Cliente.newCliente();
 
+  constructor(private service: ClienteService) {
+
+  }
+  
   salvar(){
-    console.log("Dados cliente: ", this.cliente);
+    this.service.salvar(this.cliente);
   }
 
 }
